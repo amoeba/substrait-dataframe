cb02b816be86972bc69ebdb23dc8c0b3d68d0e21
Bryce Mecum <petridish@gmail.com>
2025-02-25 15:56:09 -0800

    Move DuckDB specific code into a child backend

--- src/substrait_dataframe/backend.py
+++ src/substrait_dataframe/backend.py
@@ -2,15 +2,20 @@ class Backend:
     def __init__(self, connection):
         self.connection = connection
 
+    def sql(self, query_string):
+        return self.connection.sql(query_string)
+
+    def from_substrait(self, proto):
+        return self.connection.from_substrait(proto)
+
+
+class DuckDBBackend(Backend):
+    def __init__(self, connection):
+        super().__init__(connection)
+
     def enable(self):
         self.sql("INSTALL substrait;")
         self.sql("LOAD substrait;")
         self.sql("CREATE TABLE 'penguins' AS SELECT * FROM 'data/penguins.parquet';")
 
         return self
-
-    def sql(self, query_string):
-        return self.connection.sql(query_string)
-
-    def from_substrait(self, proto):
-        return self.connection.from_substrait(proto)
