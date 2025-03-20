b745b960d2b957481437741f05bf33b6050a6efa
Bryce Mecum <petridish@gmail.com>
2025-03-20 12:12:21 -0700

    Fix produced substrait_dataframe plans

--- testing/produce_substrait_dataframe.py
+++ testing/produce_substrait_dataframe.py
@@ -26,17 +26,77 @@ df = DataFrame(
 
 produced_path_prefix = os.path.join("testing", "produced", "substrait_dataframe")
 
+df = DataFrame(
+    relation=Relation(
+        name="penguins",
+        fields=[
+            Field("species", "string"),
+            Field("island", "string"),
+            Field("bill_length_mm", "fp64"),
+            Field("bill_depth_mm", "fp64"),
+            Field("flipper_length_mm", "i32"),
+            Field("body_mass_g", "i32"),
+            Field("sex", "string"),
+            Field("year", "i32"),
+        ],
+    )
+)
 substrait.json.write_json(
     df.to_substrait(),
     os.path.join(produced_path_prefix, "select-star-from-penguins.json"),
+)
+df = DataFrame(
+    relation=Relation(
+        name="penguins",
+        fields=[
+            Field("species", "string"),
+            Field("island", "string"),
+            Field("bill_length_mm", "fp64"),
+            Field("bill_depth_mm", "fp64"),
+            Field("flipper_length_mm", "i32"),
+            Field("body_mass_g", "i32"),
+            Field("sex", "string"),
+            Field("year", "i32"),
+        ],
+    )
 )
 substrait.json.write_json(
     df.limit(10).to_substrait(),
     os.path.join(produced_path_prefix, "select-star-from-penguins-limit-10.json"),
 )
+df = DataFrame(
+    relation=Relation(
+        name="penguins",
+        fields=[
+            Field("species", "string"),
+            Field("island", "string"),
+            Field("bill_length_mm", "fp64"),
+            Field("bill_depth_mm", "fp64"),
+            Field("flipper_length_mm", "i32"),
+            Field("body_mass_g", "i32"),
+            Field("sex", "string"),
+            Field("year", "i32"),
+        ],
+    )
+)
 substrait.json.write_json(
     df.select([Field("species", "string")]).to_substrait(),
     os.path.join(produced_path_prefix, "select-species-from-penguins.json"),
+)
+df = DataFrame(
+    relation=Relation(
+        name="penguins",
+        fields=[
+            Field("species", "string"),
+            Field("island", "string"),
+            Field("bill_length_mm", "fp64"),
+            Field("bill_depth_mm", "fp64"),
+            Field("flipper_length_mm", "i32"),
+            Field("body_mass_g", "i32"),
+            Field("sex", "string"),
+            Field("year", "i32"),
+        ],
+    )
 )
 substrait.json.write_json(
     df.select([Field("island", "string"), Field("species", "string")])
@@ -46,6 +106,21 @@ substrait.json.write_json(
         produced_path_prefix,
         "select-species-from-penguins-where-island-equals-dream.json",
     ),
+)
+df = DataFrame(
+    relation=Relation(
+        name="penguins",
+        fields=[
+            Field("species", "string"),
+            Field("island", "string"),
+            Field("bill_length_mm", "fp64"),
+            Field("bill_depth_mm", "fp64"),
+            Field("flipper_length_mm", "i32"),
+            Field("body_mass_g", "i32"),
+            Field("sex", "string"),
+            Field("year", "i32"),
+        ],
+    )
 )
 substrait.json.write_json(
     df.select([Field("island", "string"), Field("species", "string")]).to_substrait(),
