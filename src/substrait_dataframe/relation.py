1f6c639e66779e22747d4b10091388feee75b80c
Bryce Mecum <petridish@gmail.com>
2025-04-01 13:09:06 -0700

    WIP: Refactor how root relation is created
    Filter doesn't work yet but will soon

--- src/substrait_dataframe/relation.py
+++ src/substrait_dataframe/relation.py
@@ -4,9 +4,12 @@ from substrait.proto import (
     PlanRel,
     Expression,
     FetchRel,
+    FilterRel,
+    ProjectRel,
     RelRoot,
     ReadRel,
     Rel,
+    RelCommon,
     NamedStruct,
     Type,
     Version,
@@ -67,60 +70,120 @@ class Relation:
         return [PlanRel(root=self.substrait_root_rel())]
 
     def substrait_root_rel(self) -> RelRoot:
-        # Handle no selection
+        # We support a few variants of plan shapes and use pattern matching to
+        # choose:
+
+        # Read (select * from foo)
+        #
+        # Project <- Read (select bar from foo)
+        # Filter <- Read (select * from foo where baz)
+        # Fetch <- Read (select * from foo limit n)
+        #
+        # Project <- Fetch <- Read
+        # Project <- Filter <- Read
+        #
+        # Project <- Fetch <- Filter <- Read
+
+        # Start with a root at the base
+        root = self.wip_read()
+
+        # If there's a filter, we add it
+        if self.current_filter is not None:
+            root = self.wip_filter(input=root)
+
+        # If there's a limit, we add it
+        if self.current_limit is not None:
+            root = self.wip_fetch(input=root)
+
+        # If there's a select, we add it
+        if self.current_selection is not None:
+            root = self.wip_project(input=root)
+
+        # # TODO: Can remove this I think
+        # # # Handle no selection
+        # if self.current_selection is None:
+        #     self.current_selection = self.fields
+
+        # # To decide on the overal Plan structure, we essentially are just using
+        # # pattern matching which isn't very flexible and, as you can see, we
+        # # only support two very simple patterns at the moment.
+        # if self.current_limit is not None:
+        #     return self.substrait_root_fetch(input=self.wip_read())
+        # else:
+        #     return self.substrait_root_read()
+
         if self.current_selection is None:
             self.current_selection = self.fields
 
-        # To decide on the overal Plan structure, we essentially are just using
-        # pattern matching which isn't very flexible and, as you can see, we
-        # only support two very simple patterns at the moment.
-        if self.current_limit is not None:
-            return self.substrait_root_fetch()
-        else:
-            return self.substrait_root_read()
+        root_names = [field.name for field in self.current_selection]
 
-    def substrait_root_fetch(self) -> RelRoot:
+        return RelRoot(names=root_names, input=root)
+
+    def substrait_root_fetch(self, input) -> RelRoot:
         return RelRoot(
             names=[field.name for field in self.current_selection],
-            input=Rel(
-                fetch=FetchRel(
-                    input=Rel(
-                        read=ReadRel(
-                            named_table=ReadRel.NamedTable(names=[self.name]),
-                            base_schema=NamedStruct(
-                                names=[field.name for field in self.fields],
-                                struct=Type.Struct(
-                                    types=[field.type for field in self.fields],
-                                    nullability=Type.Nullability.NULLABILITY_REQUIRED,
-                                ),
-                            ),
-                            projection=self.substrait_project(),
-                            filter=self.substrait_filter(),
-                        ),
-                    ),
-                    offset=0,
-                    count=self.current_limit,
-                )
-            ),
+            input=self.wip_fetch(input),
         )
 
     def substrait_root_read(self) -> RelRoot:
         return RelRoot(
             names=[field.name for field in self.current_selection],
-            input=Rel(
-                read=ReadRel(
-                    named_table=ReadRel.NamedTable(names=[self.name]),
-                    base_schema=NamedStruct(
-                        names=[field.name for field in self.fields],
-                        struct=Type.Struct(
-                            types=[field.type for field in self.fields],
-                            nullability=Type.Nullability.NULLABILITY_REQUIRED,
-                        ),
+            input=self.wip_read(),
+        )
+
+    def wip_fetch(self, input: Rel) -> Rel:
+        return Rel(
+            fetch=FetchRel(
+                input=input,
+                offset=0,
+                count=self.current_limit,
+            )
+        )
+
+    # This is always a terminal node
+    def wip_read(self) -> Rel:
+        return Rel(
+            read=ReadRel(
+                named_table=ReadRel.NamedTable(names=[self.name]),
+                base_schema=NamedStruct(
+                    names=[field.name for field in self.fields],
+                    struct=Type.Struct(
+                        types=[field.type for field in self.fields],
+                        nullability=Type.Nullability.NULLABILITY_REQUIRED,
                     ),
-                    projection=self.substrait_project(),
-                    filter=self.substrait_filter(),
                 ),
             ),
+        )
+
+    def wip_filter(self, input: Rel) -> Rel:
+        return Rel(filter=FilterRel())
+
+    def wip_project(self, input: Rel) -> Rel:
+        return Rel(
+            project=ProjectRel(
+                common=RelCommon(
+                    direct=RelCommon.Direct(),
+                    emit=RelCommon.Emit(
+                        output_mapping=[
+                            self.fields.index(field) for field in self.current_selection
+                        ]
+                    ),
+                ),
+                input=input,
+                expressions=[
+                    Expression(
+                        selection=Expression.FieldReference(
+                            direct_reference=Expression.ReferenceSegment(
+                                struct_field=Expression.ReferenceSegment.StructField(
+                                    field=self.fields.index(field)
+                                )
+                            ),
+                            root_reference=Expression.FieldReference.RootReference(),
+                        )
+                    )
+                    for field in self.current_selection
+                ],
+            )
         )
 
     def substrait_filter(self):
