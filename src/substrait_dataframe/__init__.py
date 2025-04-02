3435bbeb6a0e705c1b3584db8cea5821e1b316c9
Bryce Mecum <petridish@gmail.com>
2025-04-02 11:40:30 -0700

    Add __all__ to inits

--- src/substrait_dataframe/__init__.py
+++ src/substrait_dataframe/__init__.py
@@ -3,3 +3,12 @@ from .dataframe import DataFrame
 from .expression import Expression
 from .field import Field
 from .relation import Relation
+
+__all__ = [
+    "DataFrame",
+    "Expression",
+    "Field",
+    "Relation",
+    "DuckDBBackend",
+    "DatafusionBackend",
+]
--- src/substrait_dataframe/backends/__init__.py
+++ src/substrait_dataframe/backends/__init__.py
@@ -2,6 +2,4 @@ from substrait_dataframe.backends.backend import Backend
 from substrait_dataframe.backends.duckdb import DuckDBBackend
 from substrait_dataframe.backends.datafusion import DatafusionBackend
 
-Backend = Backend
-DuckDBBackend = DuckDBBackend
-DatafusionBackend = DatafusionBackend
+__all__ = ["Backend", "DuckDBBackend", "DatafusionBackend"]
